6654662eec649be46a7b517cfd60012c81efd1ec
Pedro Lemos <pedroemlemos@gmail.com>
2020-05-04 02:50:50 -0300

    Listing input files

--- src/main/java/sales/batch/watcher/App.java
+++ src/main/java/sales/batch/watcher/App.java
@@ -7,29 +7,49 @@ import sales.batch.watcher.model.Sale;
 import sales.batch.watcher.model.SaleHistory;
 import sales.batch.watcher.model.Salesman;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class App {
 
     public static void main(String[] args) {
-        String fileName = "0.txt";
-        String filePath = System.getenv("HOME") + "/data/in/" + fileName;
-        SaleHistoryDAO loader = new SaleHistoryDAO();
-        SaleHistory history = loader.process(filePath);
+        String inputPath = System.getenv("HOME") + "/data/in/";
+        String outputPath = System.getenv("HOME") + "/data/out/";
 
-        for (Salesman salesman : history.getSalesmen()) {
-            System.out.println(salesman);
+        try (Stream<Path> walk = Files.walk(Paths.get(inputPath))) {
+            List<String> inputFilesPaths = walk.filter(Files::isRegularFile)
+                .map(x -> x.toString())
+                .collect(Collectors.toList());
+
+            inputFilesPaths.forEach((String filePath) -> {
+                System.out.println("FILE NAME: " + filePath);
+                SaleHistoryDAO loader = new SaleHistoryDAO();
+                SaleHistory history = loader.process(filePath);
+
+                for (Salesman salesman : history.getSalesmen()) {
+                    System.out.println(salesman);
+                }
+
+                for (Client client : history.getClients()) {
+                    System.out.println(client);
+                }
+
+                for (Sale sale : history.getSales()) {
+                    System.out.println(sale);
+                }
+
+                System.out.println("Salesmen Quantity: " + SaleHistoryAnalysis.salesmenQuantity(history));
+                System.out.println("Clients Quantity: " + SaleHistoryAnalysis.clientsQuantity(history));
+                System.out.println("Most Expensive Sale: " + SaleHistoryAnalysis.mostExpensiveSale(history));
+                System.out.println("Worst Salesman: " + SaleHistoryAnalysis.worstSalesman(history));
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-        for (Client client : history.getClients()) {
-            System.out.println(client);
-        }
-
-        for (Sale sale : history.getSales()) {
-            System.out.println(sale);
-        }
-
-        System.out.println("Salesmen Quantity: " + SaleHistoryAnalysis.salesmenQuantity(history));
-        System.out.println("Clients Quantity: " + SaleHistoryAnalysis.clientsQuantity(history));
-        System.out.println("Most Expensive Sale: " + SaleHistoryAnalysis.mostExpensiveSale(history));
-        System.out.println("Worst Salesman: " + SaleHistoryAnalysis.worstSalesman(history));
     }
 }
