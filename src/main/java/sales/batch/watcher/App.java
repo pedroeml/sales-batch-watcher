fb3f894559ab066612bd51593fa023db71df3faf
Pedro Lemos <pedroemlemos@gmail.com>
2020-05-04 09:52:46 -0300

    Updating prototype app

--- src/main/java/sales/batch/watcher/App.java
+++ src/main/java/sales/batch/watcher/App.java
@@ -1,55 +1,47 @@
 package sales.batch.watcher;
 
-import sales.batch.watcher.business.SaleHistoryAnalysis;
+import sales.batch.watcher.business.SaleHistoryReport;
+import sales.batch.watcher.dao.DirectoryUtils;
 import sales.batch.watcher.dao.SaleHistoryDAO;
-import sales.batch.watcher.model.Client;
-import sales.batch.watcher.model.Sale;
 import sales.batch.watcher.model.SaleHistory;
-import sales.batch.watcher.model.Salesman;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class App {
 
     public static void main(String[] args) {
-        String inputPath = System.getenv("HOME") + "/data/in/";
-        String outputPath = System.getenv("HOME") + "/data/out/";
+        App.loadInputFiles();
+        App.watchNewFiles();
+    }
 
-        try (Stream<Path> walk = Files.walk(Paths.get(inputPath))) {
-            List<String> inputFilesPaths = walk.filter(Files::isRegularFile)
-                .map(x -> x.toString())
-                .collect(Collectors.toList());
+    private static void watchNewFiles() {
+        // TODO: implementation
+    }
 
-            inputFilesPaths.forEach((String filePath) -> {
-                System.out.println("FILE NAME: " + filePath);
-                SaleHistoryDAO loader = new SaleHistoryDAO();
-                SaleHistory history = loader.process(filePath);
+    private static void loadInputFiles() {
+        List<String> inputFilesPaths = DirectoryUtils.listFilesInDir(DirectoryUtils.INPUT_PATH);
+        App.processUnprocessedFiles(inputFilesPaths);
+    }
 
-                for (Salesman salesman : history.getSalesmen()) {
-                    System.out.println(salesman);
-                }
+    private static void processUnprocessedFiles(List<String> inputFilesPaths) {
+        List<String> unprocessdFiles = DirectoryUtils.filterUnprocessedFiles(inputFilesPaths);
+        unprocessdFiles.forEach(App::processFile);
+    }
 
-                for (Client client : history.getClients()) {
-                    System.out.println(client);
-                }
+    private static void processFile(String filePath) {
+        String outputFilePath = DirectoryUtils.renameInputPathToOutput(filePath);
 
-                for (Sale sale : history.getSales()) {
-                    System.out.println(sale);
-                }
+        System.out.println("INPUT FILE NAME: " + filePath);
+        System.out.println("OUTPUT FILE NAME: " + outputFilePath);
 
-                System.out.println("Salesmen Quantity: " + SaleHistoryAnalysis.salesmenQuantity(history));
-                System.out.println("Clients Quantity: " + SaleHistoryAnalysis.clientsQuantity(history));
-                System.out.println("Most Expensive Sale: " + SaleHistoryAnalysis.mostExpensiveSale(history));
-                System.out.println("Worst Salesman: " + SaleHistoryAnalysis.worstSalesman(history));
-            });
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        SaleHistoryDAO loader = new SaleHistoryDAO();
+        SaleHistory history = loader.process(filePath);
+
+        List<String> lines = SaleHistoryReport.generateReportLines(history);
+        lines.forEach(System.out::println);
+
+        DirectoryUtils.writeLinesToFile(outputFilePath, lines);
+
+        System.out.println();
     }
 }
