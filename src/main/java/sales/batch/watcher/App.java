420aae5635042fa44455043e7595ce336bf4ff71
Pedro Lemos <pedroemlemos@gmail.com>
2020-05-04 02:29:35 -0300

    Minor update on prototype app

--- src/main/java/sales/batch/watcher/App.java
+++ src/main/java/sales/batch/watcher/App.java
@@ -1,5 +1,6 @@
 package sales.batch.watcher;
 
+import sales.batch.watcher.business.SaleHistoryAnalysis;
 import sales.batch.watcher.dao.SaleHistoryDAO;
 import sales.batch.watcher.model.Client;
 import sales.batch.watcher.model.Sale;
@@ -25,5 +26,10 @@ public class App {
         for (Sale sale : history.getSales()) {
             System.out.println(sale);
         }
+
+        System.out.println("Salesmen Quantity: " + SaleHistoryAnalysis.salesmenQuantity(history));
+        System.out.println("Clients Quantity: " + SaleHistoryAnalysis.clientsQuantity(history));
+        System.out.println("Most Expensive Sale: " + SaleHistoryAnalysis.mostExpensiveSale(history));
+        System.out.println("Worst Salesman: " + SaleHistoryAnalysis.worstSalesman(history));
     }
 }
